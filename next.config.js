70761365fca848f4380a76b4d88b09138322f5f5
.clxsx <86518186+clxsx@users.noreply.github.com>
2026-05-17 20:59:25 +0800

    d

--- next.config.js
+++ next.config.js
@@ -5,4 +5,10 @@ const withNextra = nextra({
   themeConfig: './theme.config.tsx'
 })
 
-export default withNextra()
+export default withNextra({
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: '/sentinel-docs', // your repo name
+})
